6e7a034bc50f42f7eaf1d6342800e4a78529c45e
Vitor Alan de Lima Santos <vitoralannl@gmail.com>
2021-09-23 18:09:51 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter'),
+    );
+  }
+}
